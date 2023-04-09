bd9b8c2cf4acdba1710fe26d99ebdf55e1936b0b
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-04-10 03:54:36 +0600

    display Tshirts and style components

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            <h2>Order Summary</h2>
+            
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import TShirt from '../TShirt/TShirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
+const Home = () => {
+    const tshirts = useLoaderData();
+    const {cart, setCart} = useState([]);
+
+    const handleAddToCart = tshirt => {
+        console.log(tshirt);
+    }
+
+     return (
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+                {
+                    tshirts.map(tshirt => <TShirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
+                    ></TShirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
